57ccb022d5bc35ee320b91763434c16c1382dd32
Deven96 <diretnandomnan@gmail.com>
2018-11-18 03:06:28 +0100

    predictor create methods

--- predict.py
+++ predict.py
@@ -2,13 +2,13 @@ import os
 import numpy as np 
 import pandas as pd
 from keras.preprocessing import image as kimage
-from keras.applications.vgg16 import preprocess_input
+from keras.models import load_model
     
 PATH = os.getcwd()
 #path of test and train folders are located a directory above
 TEST_PATH = os.path.join(os.path.split(PATH)[0], 'test')
 TEST_BATCH = os.listdir(os.path.join(TEST_PATH, 'test_images'))
-
+MODEL = os.path.join(PATH, 'fashion_mnist_trained.h5')
 
 
 def load_test_images():
@@ -22,26 +22,46 @@ def load_test_images():
     batch_size=1,
     shuffle=False
         )
-    # x_test = []
-    # for sample in TEST_BATCH:
-    #     x_test.append(sample[:-4])
+    x_test = []
+    for sample in TEST_BATCH:
+        # remove '.jpg'
+        x_test.append(sample[:-4])
         
-    # # creating dataframe to be used
-    # df = pd.DataFrame(x_test, columns=[''])
+    # creating dataframe to be used
+    df = pd.DataFrame(x_test, columns=['ImageID'])
 
     return (df, test_generator)
 
-def create_csv():
-    """create csv from predicted dataframe"""
-    pass
+def create_csv(initial_df, classes):
+    """create csv from predicted dataframe
+    
+    initial_df: dataframe containing 'ImageCategory' only
+    classes: list of yhat predictions
+    
+    returns:
+        location: full dataframe"""
+    
+    classes = pd.Series(classes)
+    initial_df['Category'] = classes.values
+    initial_df.to_csv('predictions.csv')
+    return initial_df
 
 def model():
     """replicate model by loading architecture and weights"""
-    pass
+    model = load_model(MODEL)
+    return model
 
-def predict():
-    """output predicted labels for test"""
-    pass
+def predict(model, test_data):
+    """output predicted labels for test
+    
+    model: keras model
+    test_data: numerical stream of images to predict
+
+    returns:
+        classes: y hat predictions
+    """
+    classes = model.predict_classes(test_data) 
+    return classes
 
 if __name__=="__main__":
     load_test_images()
